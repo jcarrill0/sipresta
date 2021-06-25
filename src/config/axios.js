d564cb5f7e76f7a7ab3a1e306a226abaa3e8788e
jcarrill0 <josecarrillo8@gmail.com>
2021-06-25 00:07:06 -0600

    add mockend.json

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const loanEndpoint = axios.create({
-    baseURL: 'https://60d2776c858b410017b2dccf.mockapi.io/api/'
+    // baseURL: 'https://60d2776c858b410017b2dccf.mockapi.io/api/'
     // baseURL: 'https://my-json-server.typicode.com/jcarrill0/test_loandb'
+    baseURL: 'https://sipresta-d30f.restdb.io/rest/'
 });
 
 export default loanEndpoint;
